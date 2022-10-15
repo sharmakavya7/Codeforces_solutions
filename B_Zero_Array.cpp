68b38c60c1c8e03d684fe8b534b55e2a468212ff
sharmakavya7 <sharmakavya1002@gmail.com>
2022-10-15 23:50:21 +0530

    Greedy

---
+++ B_Zero_Array.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstring>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+using namespace std;
+ 
+# define C continue;
+# define R return
+ 
+# define D double
+# define I insert
+# define ll long long
+# define ld long double
+ 
+# define ull unsigned long long
+# define ui unsigned int
+ 
+# define pb push_back
+# define pf push_front
+ 
+# define vi vector < int >
+# define vc vector < char >
+# define vs vector < string >
+# define vb vector < bool >
+# define vd vector < D >
+# define vll vector < ll >
+# define vull vector < ull >
+# define vld vector < ld >
+# define PQ priority_queue
+ 
+# define vvi vector < vector < int > >
+# define vvb vector < vector < bool > >
+# define vvc vector < vector < char > >
+# define vvs vector < vs >
+# define vvll vector < vector < ll > >
+# define vvd vector < vector < D > >
+# define vvld vector < vector < ld > >
+ 
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) v.begin() , v.end() , comp
+# define allrevcomp(v) v.rbegin() , v.rend() , comp
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+ 
+# define vpld vector < pld >
+# define vpii vector < pii >
+# define vpll vector < pll >
+# define vpDD vector < pDD >
+ 
+# define vvpii vector < vector < pii > >
+# define F first
+# define S second
+# define mp make_pair
+# define line cout<<"\n";
+
+const int N = 2e5 + 5;
+ll n, maxi, sum=0;
+// vi a;
+
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        ll t;
+        cin>>t;
+        // a.push_back(t);
+        sum += t;
+        maxi = max(maxi, t);
+    }
+
+    if(sum % 2 != 0 || 2*maxi > sum) {
+        cout << "NO" << endl;
+        return 0;
+    }
+    cout << "YES" << endl;
+    return 0; 
+}
