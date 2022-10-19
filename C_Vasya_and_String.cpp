e13016942567a5d1b29f7e920f62f298a45e0986
sharmakavya7 <sharmakavya1002@gmail.com>
2022-10-19 18:25:41 +0530

    Greedy

---
+++ C_Vasya_and_String.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <cstring>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+using namespace std;
+ 
+# define C continue;
+# define R return
+ 
+# define D double
+# define I insert
+# define ll long long
+# define ld long double
+ 
+# define ull unsigned long long
+# define ui unsigned int
+ 
+# define pb push_back
+# define pf push_front
+ 
+# define vi vector < int >
+# define vc vector < char >
+# define vs vector < string >
+# define vb vector < bool >
+# define vd vector < D >
+# define vll vector < ll >
+# define vull vector < ull >
+# define vld vector < ld >
+# define PQ priority_queue
+ 
+# define vvi vector < vector < int > >
+# define vvb vector < vector < bool > >
+# define vvc vector < vector < char > >
+# define vvs vector < vs >
+# define vvll vector < vector < ll > >
+# define vvd vector < vector < D > >
+# define vvld vector < vector < ld > >
+ 
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) v.begin() , v.end() , comp
+# define allrevcomp(v) v.rbegin() , v.rend() , comp
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+# define pld pair < ld , ld >
+# define pDD pair < D , D >
+ 
+# define vpld vector < pld >
+# define vpii vector < pii >
+# define vpll vector < pll >
+# define vpDD vector < pDD >
+ 
+# define vvpii vector < vector < pii > >
+# define F first
+# define S second
+# define mp make_pair
+# define line cout<<"\n";
+#define fast ios_base::sync_with_stdio(false); cin.tie(0);
+
+int n, k, a, b, cnt=0;
+string s;
+
+void solve() {
+    cin>>n>>k>>s;
+    for(int i=0,j=0;i<n;i++) {
+		if(s[i]=='a') {
+            a++;
+        }
+        else b++;
+        
+        while(min(a, b) > k) {
+            if(s[j] =='a') {
+                a--;
+            } 
+            else {
+                b--;
+            }
+			j++;
+		}
+        cnt=max(cnt, a + b);
+	}
+	cout << cnt; line
+}
+
+int main() {
+    solve();
+    return 0;
+}
+
