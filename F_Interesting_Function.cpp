1efce82ec2abcb54a643f849a011709db16d9937
sharmakavya7 <sharmakavya1002@gmail.com>
2022-10-27 14:45:54 +0530

    Greedy

--- F_Interesting_Function.cpp
+++ F_Interesting_Function.cpp
@@ -18,60 +18,24 @@
 #include <stack>
 using namespace std;
  
-# define C continue;
-# define R return
- 
 # define D double
 # define I insert
 # define ll long long
 # define ld long double
- 
-# define ull unsigned long long
-# define ui unsigned int
- 
-# define pb push_back
-# define pf push_front
- 
-# define vi vector < int >
-# define vc vector < char >
-# define vs vector < string >
-# define vb vector < bool >
-# define vd vector < D >
-# define vll vector < ll >
-# define vull vector < ull >
-# define vld vector < ld >
-# define PQ priority_queue
- 
-# define vvi vector < vector < int > >
-# define vvb vector < vector < bool > >
-# define vvc vector < vector < char > >
-# define vvs vector < vs >
-# define vvll vector < vector < ll > >
-# define vvd vector < vector < D > >
-# define vvld vector < vector < ld > >
- 
+
 # define all(v) (v).begin() , (v).end()
 # define allrev(v) (v).rbegin() , (v).rend()
-# define allcomp(v) v.begin() , v.end() , comp
-# define allrevcomp(v) v.rbegin() , v.rend() , comp
+# define allcomp(v) (v).begin() , (v).end() , comp
  
 # define pii pair < int , int >
 # define pll pair < ll , ll >
-# define pld pair < ld , ld >
-# define pDD pair < D , D >
- 
-# define vpld vector < pld >
-# define vpii vector < pii >
-# define vpll vector < pll >
-# define vpDD vector < pDD >
- 
-# define vvpii vector < vector < pii > >
+
 # define F first
 # define S second
 # define mp make_pair
 # define line cout<<"\n";
-const int mod= (int)1e9+7;
 # define fast ios_base::sync_with_stdio(false); cin.tie(0);
+const int mod= (int)1e9+7;
 ll __gcd(ll a,ll b) {
 	if(a!=0)return __gcd(b%a,a);
 	return b;
