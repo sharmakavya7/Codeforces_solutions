182dc07eaf0575b6279c9d6b90191f2d6d2dd607
sharmakavya7 <sharmakavya1002@gmail.com>
2022-11-12 23:24:52 +0530

    Speed training 800 rated

---
+++ A_Design_Tutorial_Learn_from_Math.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <cstring>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+using namespace std;
+ 
+# define D double
+# define I insert
+# define ll long long
+# define ld long double
+
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) (v).begin() , (v).end() , comp
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+
+# define F first
+# define S second
+# define mp make_pair
+# define line cout<<"\n";
+# define fast ios_base::sync_with_stdio(false); cin.tie(0);
+
+const int mod = (int)1e9+7;
+ll __gcd(ll a,ll b) { if(a!=0)return __gcd(b%a,a); return b; }
+bool comp(const pair<int,int> &a, const pair<int,int> &b) { return (a.second < b.second); }
+void print(vector<int> vec){ for(int i=0; i<vec.size(); i++) {cout << vec[i]<<" ";} cout<<endl;}
+
+const int N = 1e5 + 2;
+
+ll n, k, l, p, q, m, d;
+string s, t;
+// vector<int>v, v1, v2;
+int v[1004], v1[10000], v2[10000];
+int cnt = 0, sum = 0, res = 1, ans = 0;
+int matrix[5][5];
+
+void solve() {
+    cin >> n;
+    if(n % 2 != 0) {
+        cout << n - 9 << " 9";
+    } else {
+        cout << n - 4 << " 4";
+    }
+}
+
+int main() {
+    fast;
+    // ll t;
+    // cin >> t;
+    // while(t--) {
+    //     solve();
+    // }
+    solve();
+    return 0;
+}
---
+++ A_Yet_Another_Two_Integers_Problem.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <cstring>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+using namespace std;
+ 
+# define D double
+# define I insert
+# define ll long long
+# define ld long double
+
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) (v).begin() , (v).end() , comp
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+
+# define F first
+# define S second
+# define mp make_pair
+# define line cout<<"\n";
+# define fast ios_base::sync_with_stdio(false); cin.tie(0);
+
+const int mod = (int)1e9+7;
+ll __gcd(ll a,ll b) { if(a!=0)return __gcd(b%a,a); return b; }
+bool comp(const pair<int,int> &a, const pair<int,int> &b) { return (a.second < b.second); }
+void print(vector<int> vec){ for(int i=0; i<vec.size(); i++) {cout << vec[i]<<" ";} cout<<endl;}
+
+const int N = 1e5 + 2;
+
+ll n, k, l, p, q, m, d;
+string s, t;
+// vector<int>v1, v2;
+int v[3], v1[10000], v2[10000];
+int cnt = 0, sum = 0, res = 1, ans = 0;
+int matrix[5][5];
+
+void solve() {
+    cin >> n >> m;
+    int dif = abs(m - n) + 9;
+    // while(dif > 10) {
+    //     dif -= 10;
+    //     cnt++;
+    //     // cout << dif <<" ";
+    // }
+    // if(dif == 0) {
+    //     cout << cnt;
+    // }
+    cout << dif / 10; line;
+}
+
+int main() {
+    fast;
+    ll t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    // solve();
+    return 0;
+}
