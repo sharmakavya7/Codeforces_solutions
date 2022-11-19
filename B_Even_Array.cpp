545af5e8f2500c6357daf993ca591351eb071903
sharmakavya7 <sharmakavya1002@gmail.com>
2022-11-19 14:41:51 +0530

    Codeforces Round #834, A to C

---
+++ B_Even_Array.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <cstring>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+using namespace std;
+ 
+# define D double
+# define I insert
+# define ll long long
+# define ld long double
+
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) (v).begin() , (v).end() , comp
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+
+# define F first
+# define S second
+# define mp make_pair
+# define line cout<<"\n";
+# define fast ios_base::sync_with_stdio(false); cin.tie(0);
+
+const int mod = (int)1e9+7;
+ll __gcd(ll a,ll b) { if(a!=0)return __gcd(b%a,a); return b; }
+bool comp(const pair<int,int> &a, const pair<int,int> &b) { return (a.second < b.second); }
+void print(vector<int> vec){ for(int i=0; i<vec.size(); i++) {cout << vec[i]<<" ";} cout<<endl;}
+
+const int N = 2e5+10;
+
+ll n, k, l, p, q, m, d, a, b;
+string s, t;
+// vector<int>v, v1, v2;
+int v[1004], v1[10000], v2[10000];
+int cnt = 0, sum = 0, res = 1, ans = 0;
+long long dp[N];
+
+
+void solve() {
+    int n;
+		cin>>n;
+		int ev=0,od=0;
+		for(int i=0,j;i<n;i++)
+		{
+			cin>>j;
+			if(i&1)ev+=(j%2==0);
+			else od += j%2;
+		}
+		if(od!=ev)od=-1;
+		cout<<od<<endl;
+}
+
+int main() {
+    fast;
+    ll t;
+    cin >> t;
+    while(t--) {
+        solve();
+        // clear_global();
+    }
+    // solve();
+    return 0;
+}
---
+++ C_Thermostat.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <cstring>
+#include <cstdlib> 
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+using namespace std;
+ 
+# define D double
+# define I insert
+# define ll long long
+# define ld long double
+
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) (v).begin() , (v).end() , comp
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+
+# define F first
+# define S second
+# define mp make_pair
+# define line cout<<"\n";
+# define fast ios_base::sync_with_stdio(false); cin.tie(0);
+
+const int mod = (int)1e9+7;
+ll __gcd(ll a,ll b) { if(a!=0)return __gcd(b%a,a); return b; }
+bool comp(const pair<int,int> &a, const pair<int,int> &b) { return (a.second < b.second); }
+void print(vector<int> vec){ for(int i=0; i<vec.size(); i++) {cout << vec[i]<<" ";} cout<<endl;}
+
+const int N = 2e5+10;
+
+ll n, k, l, p, q, m, d, a, b;
+string s, t;
+vector<int> v1, v2;
+int v[1004]; // v1[1000], v2[10000];
+int cnt = 0, sum = 0, res = 1, ans = 0;
+long long dp[N];
+
+// OBSERVATION!!!!!!!!!!
+
+void solve() {
+    int l, r, x;
+    cin >> l >> r >> x >> a >> b;
+    if(a > b) {
+        swap(a, b);
+    }
+    int change = b - a;
+    if(change == 0) {
+        cout << "0"; line;
+        return;
+    }
+    if(change < x) {
+        if(a - l >= x || r - b >= x) {
+            // cnt = 2;
+            cout << "2"; line;
+        } else if(r - a >= x && b - l >= x) {
+            // cnt = 3;
+            cout <<"3"; line;
+        } else {
+            // cnt = -1;
+            cout << "-1"; line; 
+        }
+    } else {
+        // cnt = 1;
+        cout << "1"; line;
+    }
+    // cout << cnt; line;
+}
+
+void clear_global() {
+    v1.clear();
+    cnt = -1;
+    // mp.clear();
+}
+
+int main() {
+    fast;
+    ll t;
+    cin >> t;
+    while(t--) {
+        solve();
+        clear_global();
+    }
+    // solve();
+    return 0;
+}
