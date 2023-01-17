78c3b7c42ba06644d90a5ff72337daf3140653ff
sharmakavya7 <sharmakavya1002@gmail.com>
2023-01-18 01:33:03 +0530

    900-1400

---
+++ B_Death_s_Blessing.cpp
@@ -0,0 +1,85 @@
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <cstring>
+#include <cstdlib> 
+#include <ctime>
+#include <sstream>
+#include <string>
+#include <map>
+#include <climits>
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <functional>
+#include <utility>
+#include <deque>
+#include <queue>
+#include <stack>
+using namespace std;
+ 
+# define D double
+# define I insert
+# define ll long long
+# define ld long double
+
+# define all(v) (v).begin() , (v).end()
+# define allrev(v) (v).rbegin() , (v).rend()
+# define allcomp(v) (v).begin() , (v).end() , comp
+ 
+# define pii pair < int , int >
+# define pll pair < ll , ll >
+
+# define F first
+# define S second
+# define line cout<<"\n";
+# define fast ios_base::sync_with_stdio(false); cin.tie(0);
+
+const int mod = (int)1e9+7;
+// two coprimes' gcd = 1
+ll __gcd(ll a,ll b) { if(a!=0)return __gcd(b%a,a); return b; }
+bool comp(const pair<int,int> &a, const pair<int,int> &b) { return (a.second < b.second); }
+void print(vector<int> vec){ for(int i=0; i<vec.size(); i++) {cout << vec[i]<<" ";} cout<<endl;}
+void printv(int v[], int n) { for(int i=0; i<n; i++) { cout << v[i] <<" "; } line;}
+
+const int N = 2e5 + 10; 
+// int a[N][N];
+
+void solve () {
+    ll n;
+    cin>>n;
+    ll sum = 0;
+    for(int i=0; i<n; i++) {
+        int a;
+        cin>>a;
+        sum += a;
+    }
+    vector<ll> v;
+    for(ll i=0; i<n; i++){
+        int b;
+        cin >> b;
+        v.push_back(b);
+    }
+    sort(v.begin(), v.end());
+    for(int i=0; i<n-1; i++){
+        sum+=v[i];
+    }
+    cout << sum; line
+}
+
+int main() {
+    fast;
+    ll t;
+    cin >> t;
+    while(t--) {
+        solve();
+        // clear_global();
+    }
+    // solve();
+    return 0;
+}
